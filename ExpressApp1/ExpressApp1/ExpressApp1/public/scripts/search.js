63a1baa9824df7928d3a1cbae187778b4df7234e
Bich Diep <b-bidiep@microsoft.com>
2016-05-11 16:10:40 -0700

    cleaning up

--- ExpressApp1/ExpressApp1/ExpressApp1/public/scripts/search.js
+++
@@ -1,8 +0,0 @@
-﻿var url = "http://api.yummly.com/v1/api/recipes?_app_id=f0e69f2b&_app_key=805686b7d91a6de3510c1c7be77c7b4e&chicken};
-var app_id = "f0e69f2b";
-var app_key = "805686b7d91a6de3510c1c7be77c7b4e";
-function searchByIngredients(ingredients) {
-}
-
-function getRecipes(count = 10) {
-}
